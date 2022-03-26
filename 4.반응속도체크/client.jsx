e0f2577866285565b8f022a4fad8bfb526d54db9
SongGol <dldmsgus2@gmail.com>
2022-03-26 21:27:44 +0900

    :sparkles: 반응속도체크 class로 구현

--- 4.반응속도체크/client.jsx
+++ 4.반응속도체크/client.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const ReactionRate = require('./ReactionRate');
+//const ReactionRate = require('./ReactionRate');
+import ReactionRate from './ReactionRate';
 
 ReactDOM.render(<ReactionRate />, document.querySelector("#root"));
