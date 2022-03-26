81edb0ee3308ade30d1f1e33e47a6ad928b41aac
SongGol <dldmsgus2@gmail.com>
2022-03-26 13:15:37 +0900

    :sparkles: shouldComponentUpdate

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -15,11 +16,21 @@ module.exports = {
             test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react', ],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR'], //browsers list
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react', ],
                 plugins: [],
             },
         }],
     },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ],
     output: {
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
