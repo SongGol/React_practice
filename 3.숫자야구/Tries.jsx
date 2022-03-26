972b70beca95a5eea202678bf530b82109e8438a
SongGol <dldmsgus2@gmail.com>
2022-03-26 15:21:28 +0900

    :sparkles: props원소 변경 구현

--- 3.숫자야구/Tries.jsx
+++ 3.숫자야구/Tries.jsx
@@ -2,9 +2,17 @@
 const React = require('react');
 
 const Tries = ({tryInfo}) => {
+    //tryInfo.result = 'a'; //wrong
+    const [result, setState] = useState(tryInfo.result);
+    
+    const onClick = () => {
+        setResult('a');
+    };
+
     return(
         <li>
             <b>{tryInfo.try}</b> - {tryInfo.result}
+            <div onClick={onClick}>{result}</div>
         </li>
     )
 };
