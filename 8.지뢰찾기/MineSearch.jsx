0efd3c5b80a6e04a56e2a0a45f200492e2fa4f92
SongGol <dldmsgus2@gmail.com>
2022-04-02 19:45:32 +0900

    :tada: 지뢰찾기 시작

---
+++ 8.지뢰찾기/MineSearch.jsx
@@ -0,0 +1,29 @@
+import React, { useReducer } from 'react';
+import Table from './Table';
+
+const initialState = {
+    timer: "",
+    result: "",
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+const MineSearch = () => {
+    const [state, setState] = useReducer(reducer, initialState);
+
+    return (
+        <>
+            <Form />
+            <div>{state.timer}</div>
+            <Table />
+            <div>{state.result}</div>
+        </>
+    )
+};
+
+export default MineSearch;
---
+++ 8.지뢰찾기/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+import MineSearch from './MineSearch';
+
+ReactDOM.render(<MineSearch />, document.querySelector("#root"));
